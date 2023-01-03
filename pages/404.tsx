666d1139aa0c6e63cd726f731d414046969607ec
Himanshu Malviya <malviya.himansh@gmail.com>
2023-01-03 18:30:33 +0530

    ⚠️ add redirection to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,27 @@
 import Link from 'next/link'
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 const ErrorPage = () => {
+    const router = useRouter();
+    const [time, setTime] = useState(5);
+
+    useEffect(() => {
+        console.log(("onMount"));
+        setInterval(() => {
+            setTime(time => time-1);
+        }, 1000);
+
+        setTimeout(() => {
+            router.push("/");
+        }, 5000)
+    }, [])
+
     return (
         <div className='text-center'>
             <h1 className='text-3xl lg:text-5xl text-red-500 font-bold mb-5'>⚠️ Error 404 ⚠️</h1>
             <p className='text-xl lg:text-3xl font-semibold'>This page does not exist.</p>
-            <p className='text-base lg:text-xl mt-2'>Get back to <Link href='/'><span className='text-blue-600 underline'>Home</span></Link></p>
+            <p className='text-base lg:text-xl mt-2'>Returning to <Link href='/'><span className='text-blue-600 underline'>Home</span></Link> in {time} seconds...</p>
         </div>
     );
 };
