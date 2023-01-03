a019a86755bb3f049f69b299f206f264742b6b35
Himanshu Malviya <malviya.himansh@gmail.com>
2023-01-03 18:28:06 +0530

    🧁 add bakery api

---
+++ pages/api/getBakeryItems.ts
@@ -0,0 +1,55 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type BakeryItem = {
+  id: number,
+  name: string,
+  price: string,
+  image: string,
+  description: string
+}
+
+export default function hello(req: NextApiRequest, res: NextApiResponse<BakeryItem[]>) {
+  res.status(200).json([
+    {
+      id: 1,
+      name: "Sushi 🍣",
+      price: "₹450",
+      image: "",
+      description:
+        'Sushi is a Japanese dish of prepared vinegared rice, usually with some sugar and salt, accompanied by a variety of ingredients, such as seafood, often raw, and vegetables. Styles of sushi and its presentation vary widely, but the one key ingredient is "sushi rice," also referred to as shari, or sumeshi.',
+    },
+    {
+      id: 2,
+      name: "Pretzel 🥨",
+      price: "₹699",
+      image: "",
+      description:
+        "A pretzel, from German pronunciation, standard German: Breze is a type of baked bread made from dough that is commonly shaped into a knot. The traditional pretzel shape is a distinctive symmetrical form, with the ends of a long strip of dough intertwined and then twisted back onto itself in a particular way.",
+    },
+    {
+      id: 3,
+      name: "Bagel 🥯",
+      price: "₹75",
+      image: "",
+      description:
+        "A bagel is a bread roll originating in the Jewish communities of Poland. It is traditionally shaped by hand into a roughly hand-sized ring from yeasted wheat dough that is first boiled for a short time in water and then baked. The result is a dense, chewy, doughy interior with a browned and sometimes crisp exterior.",
+    },
+    {
+      id: 4,
+      name: "Baguette Bread 🥖",
+      price: "₹79",
+      image: "",
+      description:
+        "A baguette is a long, thin type of bread of French origin that is commonly made from basic lean dough. It is distinguishable by its length and crisp crust. A baguette has a diameter of about 5 to 6 centimetres and a usual length of about 65 cm, although a baguette can be up to 1 m long.",
+    },
+    {
+      id: 5,
+      name: "Pancakes 🥞",
+      price: "₹225",
+      image: "",
+      description:
+        "A pancake is a flat cake, often thin and round, prepared from a starch-based batter that may contain eggs, milk and butter and cooked on a hot surface such as a griddle or frying pan, often frying with oil or butter. It is a type of batter bread.",
+    },
+  ]);
+}
