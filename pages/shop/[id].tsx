71af5682e770f26718189b475479b684c55ccb43
Himanshu Malviya <malviya.himansh@gmail.com>
2023-01-04 19:40:55 +0530

    🐛 fix router bug

--- pages/shop/[id].tsx
+++ pages/shop/[id].tsx
@@ -28,12 +28,13 @@ export async function getStaticProps(context: any) {
 }
 
 const BakeryItem = ({ product }: any) => {
+    const router = useRouter();
     return (
         <div>
             <h1 className="text-xl lg:text-3xl mb-5 font-semibold">{product?.name}</h1>
             <p className="text-base lg:text-lg my-2 font-bold">{product?.price}</p>
             <p className="text-base lg:text-lg">{product?.description}</p>
-            <button onClick={() => useRouter().push('/404')} className="border-2 border-[#C58940] hover:bg-[#C58940] transition duration-100 ease hover:text-[#f2f2f2] py-2 px-8 mt-5 rounded-md" >Buy</button>
+            <button onClick={() => router.push('/404')} className="border-2 border-[#C58940] hover:bg-[#C58940] transition duration-100 ease hover:text-[#f2f2f2] py-2 px-8 mt-5 rounded-md" >Buy</button>
         </div>
     );
 }
